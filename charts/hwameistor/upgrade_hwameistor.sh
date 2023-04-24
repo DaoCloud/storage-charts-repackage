b9d116d8836fbd0a102f9cf9f28b8e82e11e6935
mingming.zhou <mingming.zhou@daocloud.io>
2023-04-24 15:50:57 +0800

    upgrade hwameistor to v0.9.3

---
+++ charts/hwameistor/upgrade_hwameistor.sh
@@ -0,0 +1,60 @@
+## This script is only used for upgrade hwameistor version 
+## according to version released in offical charts rep(https://hwameistor.io)
+
+# /bin/bash
+
+releaseVersion=$1
+
+[ "$releaseVersion" == "" ] && \
+echo "release version must be specified!(example: bash upgrade_hwameistor.sh v0.9.3)" && \
+exit 1
+
+rootProgram="$(dirname $PWD)/.."
+# Step1: download specified version charts
+
+echo "Step1: download specified version charts"
+
+pushd . &>/dev/null
+
+[ -e "hwameistor" ] && rm -rf hwameistor/ 
+
+oldVersion=$(ss=$(grep "VERSION" config) && echo ${ss#*=})
+echo "oldVersion: $oldVersion"
+OS=$(uname)
+if [ "$OS" == "Darwin" ];then 
+	sed -i '' "s/$oldVersion/$releaseVersion/g" config
+elif [ "$OS" == "Linux" ] 
+    sed -i "s/$oldVersion/$releaseVersion/g" config
+then 
+    echo "Unsupport OS $OS" && exit 1
+fi
+
+cd $rootProgram && make -e PROJECT=hwameistor &>/dev/null
+
+popd &>/dev/null
+
+[ ! -e "./hwameistor/" ] && echo "failed to update hwameistor charts!" && exit 1
+
+# Step2: copy custom files from hwameistor/parent
+
+echo "Step2: copy custom files from hwameistor/parent"
+
+cp parent/* ./hwameistor
+
+[ -e "./hwameistor/crds/module.go" ] && rm -rf hwameistor/crds/module.go
+
+! grep 'keywords' ./hwameistor/Charts.yaml &>/dev/null && \
+echo "keywords:
+  - storage
+  - local
+  - block" >> ./hwameistor/Chart.yaml
+
+echo "
+Updaate hwameistor $releaseVersion success!
+
+Changed files:
+"
+
+git status -s
+
+# Step3: check charts correctly by `helm template`
